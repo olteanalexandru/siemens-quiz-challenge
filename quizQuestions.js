5cf3fc7feea14e9e22b4ae3335274d031eaf8324
Alexandru Oltean <oltean.alexandru11@gmail.com>
2023-12-20 18:11:40 +0000

    quiz questions

---
+++ quizQuestions.js
@@ -0,0 +1,554 @@
+let questions = [
+    {
+        id: 1,
+        question: "What is the capital of Romania?",
+        answer: "Bucharest",
+        options: [
+            "Bucharest",
+            "Cluj-Napoca",
+            "Timisoara",
+            "Iasi"
+        ]
+    },
+    {
+        id: 2,
+        question: "What is the capital of France?",
+        answer: "Paris",
+        options: [
+            "Paris",
+            "Lyon",
+            "Marseille",
+            "Toulouse"
+        ]
+    },
+    {
+        id: 3,
+        question: "What is the capital of Germany?",
+        answer: "Berlin",
+        options: [
+            "Berlin",
+            "Hamburg",
+            "Munich",
+            "Cologne"
+        ]
+    },
+    {
+        id: 4,
+        question: "What is the capital of Italy?",
+        answer: "Rome",
+        options: [
+            "Rome",
+            "Milan",
+            "Naples",
+            "Turin"
+        ]
+    },
+    {
+        id: 5,
+        question: "What is the capital of Spain?",
+        answer: "Madrid",
+        options: [
+            "Madrid",
+            "Barcelona",
+            "Valencia",
+            "Seville"
+        ]
+    },
+    {
+        id: 6,
+        question: "What is the capital of Portugal?",
+        answer: "Lisbon",
+        options: [
+            "Lisbon",
+            "Porto",
+            "Braga",
+            "Coimbra"
+        ]
+    },
+    {
+        id: 7,
+        question: "What is the capital of Greece?",
+        answer: "Athens",
+        options: [
+            "Athens",
+            "Thessaloniki",
+            "Patras",
+            "Heraklion"
+        ]
+    },
+    {
+        id: 8,
+        question: "What is the capital of Norway?",
+        answer: "Oslo",
+        options: [
+            "Oslo",
+            "Bergen",
+            "Trondheim",
+            "Stavanger"
+        ]
+    },
+    {
+        id: 9,
+        question: "What is the capital of Sweden?",
+        answer: "Stockholm",
+        options: [
+            "Stockholm",
+            "Gothenburg",
+            "Malmo",
+            "Uppsala"
+        ]
+    },
+    {
+        id: 10,
+        question: "What is the capital of Finland?",
+        answer: "Helsinki",
+        options: [
+            "Helsinki",
+            "Espoo",
+            "Tampere",
+            "Vantaa"
+        ]
+    },
+    {
+        id: 11,
+        question: "What is the capital of Denmark?",
+        answer: "Copenhagen",
+        options: [
+            "Copenhagen",
+            "Aarhus",
+            "Odense",
+            "Aalborg"
+        ]
+    },
+    {
+        id: 12,
+        question: "What is the capital of Iceland?",
+        answer: "Reykjavik",
+        options: [
+            "Reykjavik",
+            "Kopavogur",
+            "Hafnarfjordur",
+            "Akureyri"
+        ]
+    },
+    {
+        id: 13,
+        question: "What is the capital of Ireland?",
+        answer: "Dublin",
+        options: [
+            "Dublin",
+            "Cork",
+            "Galway",
+            "Limerick"
+        ]
+    },
+    {
+        id: 14,
+        question: "What is the capital of the United Kingdom?",
+        answer: "London",
+        options: [
+            "London",
+            "Manchester",
+            "Birmingham",
+            "Liverpool"
+        ]
+    },
+    {
+        id: 15,
+        question: "What is the capital of the Netherlands?",
+        answer: "Amsterdam",
+        options: [
+            "Amsterdam",
+            "Rotterdam",
+            "The Hague",
+            "Utrecht"
+        ]
+    },
+    {
+        id: 16,
+        question: "What is the capital of Belgium?",
+        answer: "Brussels",
+        options: [
+            "Brussels",
+            "Antwerp",
+            "Ghent",
+            "Charleroi"
+        ]
+    },
+    {
+        id: 17,
+        question: "What is the capital of Luxembourg?",
+        answer: "Luxembourg City",
+        options: [
+            "Luxembourg City",
+            "Esch-sur-Alzette",
+            "Dudelange",
+            "Schifflange"
+        ]
+    },
+    {
+        id: 18,
+        question: "What is the capital of Switzerland?",
+        answer: "Bern",
+        options: [
+            "Bern",
+            "Zurich",
+            "Geneva",
+            "Basel"
+        ]
+    },
+    {
+        id: 19,
+        question: "What is the capital of Austria?",
+        answer: "Vienna",
+        options: [
+            "Vienna",
+            "Graz",
+            "Linz",
+            "Salzburg"
+        ]
+    },
+    {
+        id: 20,
+        question: "What is the capital of Hungary?",
+        answer: "Budapest",
+        options: [
+            "Budapest",
+            "Debrecen",
+            "Szeged",
+            "Miskolc"
+        ]
+    },
+    {
+        id: 21,
+        question: "What is the capital of Poland?",
+        answer: "Warsaw",
+        options: [
+            "Warsaw",
+            "Krakow",
+            "Lodz",
+            "Wroclaw"
+        ]
+    },
+    {
+        id: 22,
+        question: "What is the capital of Ukraine?",
+        answer: "Kyiv",
+        options: [
+            "Kyiv",
+            "Kharkiv",
+            "Odesa",
+            "Dnipro"
+        ]
+    },
+    {
+        id: 23,
+        question: "What is the capital of Belarus?",
+        answer: "Minsk",
+        options: [
+            "Minsk",
+            "Gomel",
+            "Mogilev",
+            "Vitebsk"
+        ]
+    },
+    {
+        id: 24,
+        question: "What is the capital of Moldova?",
+        answer: "Chisinau",
+        options: [
+            "Chisinau",
+            "Tiraspol",
+            "Balti",
+            "Bender"
+        ]
+    },
+    {
+        id: 25,
+        question: "What is the capital of Romania?",
+        answer: "Bucharest",
+        options: [
+            "Bucharest",
+            "Cluj-Napoca",
+            "Timisoara",
+            "Iasi"
+        ]
+    },
+    {
+        id: 26,
+        question: "What is the capital of Bulgaria?",
+        answer: "Sofia",
+        options: [
+            "Sofia",
+            "Plovdiv",
+            "Varna",
+            "Burgas"
+        ]
+    },
+    {
+        id: 27,
+        question: "What is the capital of Turkey?",
+        answer: "Ankara",
+        options: [
+            "Ankara",
+            "Istanbul",
+            "Izmir",
+            "Bursa"
+        ]
+    },
+    {
+        id: 28,
+        question: "What is the capital of Greece?",
+        answer: "Athens",
+        options: [
+            "Athens",
+            "Thessaloniki",
+            "Patras",
+            "Heraklion"
+        ]
+    },
+    {
+        id: 29,
+        question: "What is the capital of Cyprus?",
+        answer: "Nicosia",
+        options: [
+            "Nicosia",
+            "Limassol",
+            "Larnaca",
+            "Paphos"
+        ]
+    },
+    {
+        id: 30,
+        question: "What is the capital of Russia?",
+        answer: "Moscow",
+        options: [
+            "Moscow",
+            "Saint Petersburg",
+            "Novosibirsk",
+            "Yekaterinburg"
+        ]
+    },
+    {
+        id: 31,
+        question: "What is the capital of Kazakhstan?",
+        answer: "Nur-Sultan",
+        options: [
+            "Nur-Sultan",
+            "Almaty",
+            "Karaganda",
+            "Shymkent"
+        ]
+    },
+    {
+        id: 32,
+        question: "What is the capital of Georgia?",
+        answer: "Tbilisi",
+        options: [
+            "Tbilisi",
+            "Kutaisi",
+            "Batumi",
+            "Rustavi"
+        ]
+    },
+    {
+        id: 33,
+        question: "What is the capital of Azerbaijan?",
+        answer: "Baku",
+        options: [
+            "Baku",
+            "Ganja",
+            "Sumqayit",
+            "Mingachevir"
+        ]
+    },
+    {
+        id: 34,
+        question: "What is the capital of Armenia?",
+        answer: "Yerevan",
+        options: [
+            "Yerevan",
+            "Gyumri",
+            "Vanadzor",
+            "Vagharshapat"
+        ]
+    },
+    {
+        id: 35,
+        question: "What is the capital of Iran?",
+        answer: "Tehran",
+        options: [
+            "Tehran",
+            "Mashhad",
+            "Isfahan",
+            "Karaj"
+        ]
+    },
+    {
+        id: 36,
+        question: "What is the capital of Iraq?",
+        answer: "Baghdad",
+        options: [
+            "Baghdad",
+            "Basra",
+            "Sulaymaniyah",
+            "Mosul"
+        ]
+    },
+    {
+        id: 37,
+        question: "What is the capital of Saudi Arabia?",
+        answer: "Riyadh",
+        options: [
+            "Riyadh",
+            "Jeddah",
+            "Mecca",
+            "Medina"
+        ]
+    },
+    {
+        id: 38,
+        question: "What is the capital of Egypt?",
+        answer: "Cairo",
+        options: [
+            "Cairo",
+            "Alexandria",
+            "Giza",
+            "Shubra El-Kheima"
+        ]
+    },
+    {
+        id: 39,
+        question: "What is the capital of Libya?",
+        answer: "Tripoli",
+        options: [
+            "Tripoli",
+            "Benghazi",
+            "Misrata",
+            "Tarhuna"
+        ]
+    },
+    {
+        id: 40,
+        question: "What is the capital of Algeria?",
+        answer: "Algiers",
+        options: [
+            "Algiers",
+            "Oran",
+            "Constantine",
+            "Annaba"
+        ]
+    },
+    {
+        id: 41,
+        question: "What is the capital of Tunisia?",
+        answer: "Tunis",
+        options: [
+            "Tunis",
+            "Sfax",
+            "Sousse",
+            "Kairouan"
+        ]
+    },
+    {
+        id: 42,
+        question: "What is the capital of Morocco?",
+        answer: "Rabat",
+        options: [
+            "Rabat",
+            "Casablanca",
+            "Fes",
+            "Tangier"
+        ]
+    },
+    {
+        id: 43,
+        question: "What is the capital of Mauritania?",
+        answer: "Nouakchott",
+        options: [
+            "Nouakchott",
+            "Nouadhibou",
+            "Kiffa",
+            "Zouerate"
+        ]
+    },
+    {
+        id: 44,
+        question: "What is the capital of Mali?",
+        answer: "Bamako",
+        options: [
+            "Bamako",
+            "Sikasso",
+            "Mopti",
+            "Segou"
+        ]
+    },
+    {
+        id: 45,
+        question: "What is the capital of Niger?",
+        answer: "Niamey",
+        options
+    :
+        [
+            "Niamey",
+            "Zinder",
+            "Maradi",
+            "Agadez"
+        ]
+    },
+    {
+        id: 46,
+        question: "What is the capital of Chad?",
+        answer: "N'Djamena",
+        options: [
+            "N'Djamena",
+            "Moundou",
+            "Sarh",
+            "Abéché"
+        ]
+    },
+    {
+        id: 47,
+        question: "What is the capital of Sudan?",
+        answer: "Khartoum",
+        options: [
+            "Khartoum",
+            "Omdurman",
+            "Nyala",
+            "Port Sudan"
+        ]
+    },
+    {
+        id: 48,
+        question: "What is the capital of South Sudan?",
+        answer: "Juba",
+        options: [
+            "Juba",
+            "Wau",
+            "Malakal",
+            "Bor"
+        ]
+    },
+    {
+        id: 49,
+        question: "What is the capital of Ethiopia?",
+        answer: "Addis Ababa",
+        options: [
+            "Addis Ababa",
+            "Dire Dawa",
+            "Mek'ele",
+            "Gondar"
+        ]
+    },
+    {
+        id: 50,
+        question: "What is the capital of Somalia?",
+        answer: "Mogadishu",
+        options: [
+            "Mogadishu",
+            "Hargeisa",
+            "Kismayo",
+            "Marka"
+        ]
+    }
+]
